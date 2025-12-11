e53f96dc3879d3a02c78690e6435e1f27db08fae
Alex O <98245982+alexo46@users.noreply.github.com>
2025-12-10 18:25:50 -0600

    Build for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
     plugins: [wasm(), topLevelAwait()],
+    base: "/CS559-Fall-P3/",
+    build: {
+        outDir: "docs",
+    },
 });
